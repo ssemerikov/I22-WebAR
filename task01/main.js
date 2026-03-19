a53909e50cd384aab584e5b1692bda6410faba99
Serhiy Semerikov <semerikov@gmail.com>
2026-03-19 13:58:21 +0200

    task01

---
+++ task01/main.js
@@ -0,0 +1,58 @@
+import * as THREE from 'three';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scene = new THREE.Scene();
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({color: "#0000FF"});
+    const cube = new THREE.Mesh(geometry, material);
+
+    cube.position.set(0, 0, -2);
+    cube.rotation.set(0, Math.PI/4, 0);
+    scene.add(cube);
+
+    const camera = new THREE.PerspectiveCamera();
+    camera.position.set(1, 1, 5);
+
+    const renderer = new THREE.WebGLRenderer({alpha: true});
+    renderer.setSize(500, 500);
+    renderer.render(scene, camera);
+
+    renderer.setAnimationLoop(() => {
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    });
+
+    // video before canvas in DOM
+
+    const video = document.createElement("video");
+
+    navigator.mediaDevices.getUserMedia({video:true})
+        .then((stream) => {
+            video.srcObject = stream;
+            video.play();
+        });
+
+    video.style.position = "absolute";
+    video.style.width = renderer.domElement.width;
+    video.style.height = renderer.domElement.height;
+    renderer.domElement.style.position = "absolute";
+
+    document.body.appendChild(video);
+    document.body.appendChild(renderer.domElement);
+
+
+    /*
+        Our AR engine should be able to compute the position and rotation of the cube in the video stream.
+
+
+    const ar = new SOME_AR_ENGINE();
+    while(true)
+    {
+        await nextVideoFrameReady();
+        const {position, rotation} = ar.computeObjectPose(video);
+        cube.position = position;
+        cube.rotation = rotation;
+    }
+    */
+});
